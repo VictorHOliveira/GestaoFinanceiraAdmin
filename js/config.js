b83cb80cc90f57a6cab31b51b3565a232f9f70fd
Victor Oliver <victor.oliver87@gmail.com>
2026-05-01 22:00:32 +0100

    Corrigir categorias: recriar config.js e ajustar add-expense/add-income

---
+++ js/config.js
@@ -0,0 +1,21 @@
+// Categories
+const CATEGORIES = [
+    "Moradia (Aluguer/Hipoteca)",
+    "Alimentacao (Supermercado/Restaurantes)",
+    "Transporte (Combustivel/Transportes Publicos)",
+    "Contas (Agua/Eletricidade/Gas/Internet)",
+    "Saude (Medico/Farmacia)",
+    "Educacao (Propinas/Cursos)",
+    "Lazer (Cinema/Viagens/Hobbies)",
+    "Compras (Roupa/Eletronicos)",
+    "Outros"
+];
+
+const INCOME_CATEGORIES = [
+    "Ordenado",
+    "Trabalho Extra",
+    "Investimentos (Dividendos/Juros)",
+    "Vendas",
+    "Rendas Recebidas",
+    "Outros Rendimentos"
+];
